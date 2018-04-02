5df1c6b987ecf3f55ad87032649fc87fb5eecb58
helloworld575 <zhichenli6@gmail.com>
2018-04-02 11:45:28 +0800

    can get Danbooru mainpage img and download

--- danbooru.py
+++ danbooru.py
@@ -1,28 +1,48 @@
 # -*- coding: UTF-8 -*-
+import requests
+import re
 
-import urllib.request
 
-# response = urllib.request.urlopen(request)
-# print(response.read().decode('utf-8'))
 class DanbooruSpider:
-    url = 'https://danbooru.donmai.us/'
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-    headers = {'User-agent': user_agent}
-    request = urllib.request.Request(url, headers=headers)
+    url = 'https://danbooru.donmai.us'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/52.0.2743.116 Safari/537.36'
+    }
+    data = {}
+    imgNum = 0
 
     def getRobots(self):
         with open('Danbooru/DanbooruRoobots.txt', 'w') as f:
-            robotRequest = urllib.request.Request(self.url + 'robots.txt', headers=self.headers)
-            f.write(urllib.request.urlopen(robotRequest).read().decode('utf-8'))
+            robotRequest = requests.get(self.url + '/robots.txt')
+            f.write(robotRequest.text)
 
     def getMainPage(self):
-        self.response = urllib.request.urlopen(self.request)
-        with open('Danbooru/DanbooruMainPage.txt','w') as f:
-            f.write(self.response.read())
+        self.response = requests.get(self.url, params=self.data, headers=self.headers)
+        print(self.response.text)
+
+    def getImgs(self, imgPage):
+        pattern = re.compile('<img itemprop="thumbnailUrl" src="/data/preview/(.*?)" ', re.S)
+        imgID = re.findall(pattern, imgPage.text)
+        for url in imgID:
+            imgUrl = self.url+'/data/'+url
+            type = re.findall(r'^.*\.(.*?)$', imgUrl)
+            print("正在写入"+imgUrl+"编号："+str(self.imgNum))
+            self.writeImg(imgUrl,type[0])
+            print("编号："+str(self.imgNum-1)+"写入完成！")
+
+    def writeImg(self, url, imgType):
+        img = requests.get(url, headers=self.headers)
+        with open('Danbooru/media/' + str(self.imgNum) + '.' + imgType, 'wb') as f:
+            f.write(img.content)
+            self.imgNum += 1
 
     def main(self):
-        self.getRobots()
-        self.getMainPage()
+        # self.getRobots()
+        # self.getMainPage()
+        r = requests.get('https://danbooru.donmai.us', headers=self.headers)
+        self.getImgs(r)
+
 
 if __name__ == '__main__':
     d = DanbooruSpider()
--- warmUp.py
+++ warmUp.py
@@ -4,13 +4,18 @@
 # request = http.request('GET','http://otome.me')
 # print(request.status)
 # print(request.data)
+#
+# import http.cookiejar, urllib.request
+# user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+# headers = {'User-agent':user_agent}
+# cookie = http.cookiejar.CookieJar()
+# handler = urllib.request.HTTPCookieProcessor(cookie)
+# opener = urllib.request.build_opener(handler)
+# response = opener.open('https://danbooru.donmai.us')
+# for item in cookie:
+#     print(item.name+"="+item.value)
 
-import http.cookiejar, urllib.request
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = {'User-agent':user_agent}
-cookie = http.cookiejar.CookieJar()
-handler = urllib.request.HTTPCookieProcessor(cookie)
-opener = urllib.request.build_opener(handler)
-response = opener.open('https://danbooru.donmai.us')
-for item in cookie:
-    print(item.name+"="+item.value)
+import requests
+r = requests.get('https://danbooru.donmai.us/')
+print(r.status_code)
+print(r.text)
